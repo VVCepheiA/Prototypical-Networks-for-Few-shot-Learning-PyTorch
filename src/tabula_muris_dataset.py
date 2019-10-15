8001d42e7010371f4a084f8536439e5c16e119e3
VVCepheiA <oyevera@gmail.com>
2019-10-15 13:13:33 -0700

    custom dataset

--- src/tabula_muris_dataset.py
+++ src/tabula_muris_dataset.py
@@ -3,25 +3,29 @@ from __future__ import print_function
 import torch.utils.data as data
 from PIL import Image
 import numpy as np
+import pandas as pd
 import shutil
 import errno
 from pprint import pprint
 import torch
 import os
+from tqdm import tqdm
 
 '''
 Inspired by https://github.com/pytorch/vision/pull/46
 '''
-
-IMG_CACHE = {}
 
 
 class TabulaMurisDataset(data.Dataset):
 
     splits_folder = os.path.join('splits', 'vinyals')
     processed_folder = 'data'
+    tabula_muris_split = {'train': ['Bladder', 'Lung', 'Kidney', 'Heart', 'Pancreas', 'Brain_Myeloid', 'Marrow',
+                                    'Mammary_Gland', 'Brain_Non-Myeloid', 'Trachea', 'Fat'],
+                          'val': ['Large_Intestine', 'Liver', 'Thymus'],
+                          'test': ['Skin', 'Tongue', 'Spleen', 'Limb_Muscle']}
 
-    def __init__(self, mode='train', root='..' + os.sep + 'dataset', transform=None, target_transform=None):
+    def __init__(self, mode='train', root='../../data/tabula_muris'):
         '''
         The items are (filename,category). The index of all the categories can be found in self.idx_classes
         Args:
@@ -29,103 +33,48 @@ class TabulaMurisDataset(data.Dataset):
         - transform: how to transform the input
         - target_transform: how to transform the target
         '''
+
         super(TabulaMurisDataset, self).__init__()
         self.root = root
-        self.transform = transform
-        self.target_transform = target_transform
+        self.mode = mode
 
-        self.classes = get_current_classes(os.path.join(
-            self.root, self.splits_folder, mode + '.txt'))
-        self.all_items = find_items(os.path.join(
-            self.root, self.processed_folder), self.classes)
+        # load self.x, self.y, self.classes, self.idx_classes
+        self.x, self.y, self.idx_classes, self.n_items = None, [], {}, 0
+        self.load_data()
 
-        self.idx_classes = index_classes(self.all_items)
-
-        paths, self.y = zip(*[self.get_path_label(pl)
-                              for pl in range(len(self))])
-
-        self.x = map(load_img, paths, range(len(paths)))
-        self.x = list(self.x)
+    def load_data(self):
+        self.idx_classes = {}
+        tensors = []
+        self.y = []
+        tissues = self.tabula_muris_split[self.mode]
+        print("Loading data...")
+        for tissue in tqdm(tissues):
+            tissue_dir = os.path.join(self.root, tissue)
+            cell_ontology_classes = os.listdir(tissue_dir)
+            for cell_ontology_class in cell_ontology_classes:
+                label = tissue + "/" + cell_ontology_class
+                self.idx_classes[label] = len(self.idx_classes)
+                path = os.path.join(tissue_dir, cell_ontology_class, "genes.csv")
+                tensor = read_csv_to_tensor(path)
+                tensors.append(tensor)
+                self.y += tensor.shape[0] * [self.idx_classes[label]]
+        self.x = torch.cat(tensors, 0)
+        self.n_items = self.x.shape[0]
+        print("Mode: {}. Loaded {} classes.".format(self.mode, len(self.idx_classes)))
+        print("X shape: {}. Y length: {}.".format(self.x.shape, len(self.y)))
 
     def __getitem__(self, idx):
         x = self.x[idx]
         return x, self.y[idx]
 
     def __len__(self):
-        return len(self.all_items)
-
-    def get_path_label(self, index):
-        '''
-        Return image path with rotation and the class id
-        '''
-        filename = self.all_items[index][0]
-        rot = self.all_items[index][-1]
-        img = str.join(os.sep, [self.all_items[index][2], filename]) + rot
-        target = self.idx_classes[self.all_items[index]
-                                  [1] + self.all_items[index][-1]]
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return img, target
+        return self.n_items
 
 
-def find_items(root_dir, classes):
-    '''
-    Find the file information for the classes.
-    Return (image_file_name, class, image_dir, rotation)
-    '''
-    retour = []
-    rots = [os.sep + 'rot000', os.sep + 'rot090', os.sep + 'rot180', os.sep + 'rot270']
-    for (root, dirs, files) in os.walk(root_dir):
-        for f in files:
-            r = root.split(os.sep)
-            lr = len(r)
-            label = r[lr - 2] + os.sep + r[lr - 1]
-            for rot in rots:
-                if label + rot in classes and (f.endswith("png")):
-                    retour.extend([(f, label, root, rot)])
-    print("== Dataset: Found %d items " % len(retour))
-    return retour
-
-
-def index_classes(items):
-    '''
-    items: (image_file_name, class, image_dir, rotation)
-    return: assigned unique int id for each class
-    '''
-    idx = {}
-    for i in items:
-        if (not i[1] + i[-1] in idx):
-            idx[i[1] + i[-1]] = len(idx)
-    print("== Dataset: Found %d classes" % len(idx))
-    return idx
-
-
-def get_current_classes(fname):
-    '''
-    return the list of classes in *.txt (the label file)
-    '''
-    with open(fname) as f:
-        classes = f.read().replace('/', os.sep).splitlines()
-    return classes
-
-
-def load_img(path, idx):
-    '''
-    Load image, rotate and resize
-    '''
-    path, rot = path.split(os.sep + 'rot')
-    if path in IMG_CACHE:
-        x = IMG_CACHE[path]
-    else:
-        x = Image.open(path)
-        IMG_CACHE[path] = x
-    x = x.rotate(float(rot))
-    x = x.resize((28, 28))
-
-    shape = 1, x.size[0], x.size[1]
-    x = np.array(x, np.float32, copy=False)
-    x = 1.0 - torch.from_numpy(x)
-    # 1 * 28 * 28
-    x = x.transpose(0, 1).contiguous().view(shape)
-
+def read_csv_to_tensor(path, trim=150):
+    df = pd.read_csv(path, header=None)
+    x = torch.from_numpy(df.to_numpy(dtype=np.float32))
+    num_data = x.shape[0]
+    x = x[:, :trim**2].view(num_data, 1, trim, trim)
     return x
+
